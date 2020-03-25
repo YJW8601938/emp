6e59c6e57181e0f8bdb21f2714b42e602ec9b439
yjw8601938 <yjw942309007@163.com>
2020-03-25 16:13:45 +0800

    "修正代码,用户名不存在有问题"

--- run_suite.py
+++ run_suite.py
@@ -10,7 +10,8 @@ suite=unittest.TestSuite()
 suite.addTest(unittest.makeSuite(TestLogin))
 suite.addTest(unittest.makeSuite(TestEmployee))
 
-report_path=os.path.dirname(os.path.abspath(__file__))+"/report/ihrm{}.html".format(time.strftime("%Y%m%d  %H%M%S"))
+# report_path=os.path.dirname(os.path.abspath(__file__))+"/report/ihrm{}.html".format(time.strftime("%Y%m%d  %H%M%S"))
+report_path=os.path.dirname(os.path.abspath(__file__))+"/report/ihrm{}.html"
 with open(report_path ,mode="wb") as f:
     from HTMLTestRunner_PY3 import HTMLTestRunner
     runner=HTMLTestRunner(f , verbosity=2,title="人力资源接口测试报告",description="这是项目实战的报告")
